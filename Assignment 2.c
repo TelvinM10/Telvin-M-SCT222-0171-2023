0eb5aa54ab5e048842744780a711fe1c8a1fecb4
TelvinM10 <telvin.kahiu@students.jkuat.ac.ke>
2025-01-24 16:55:54 +0300

    Assignment 2.c

--- Assignment 2.c
+++ Assignment 2.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a linked list node
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to create a new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
@@ -15,7 +13,6 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-// Function to print a linked list
 void printList(struct Node* head) {
     struct Node* temp = head;
     while (temp != NULL) {
@@ -25,15 +22,12 @@ void printList(struct Node* head) {
     printf("NULL\n");
 }
 
-// Function to merge two sorted linked lists
 struct Node* merge(struct Node* listA, struct Node* listB) {
-    // Create a dummy node to act as the head of the merged list
-    struct Node dummy;
+        struct Node dummy;
     struct Node* tail = &dummy;
     dummy.next = NULL;
 
-    // Merge the two lists
-    while (listA != NULL && listB != NULL) {
+        while (listA != NULL && listB != NULL) {
         if (listA->data <= listB->data) {
             tail->next = listA;
             listA = listA->next;
@@ -44,8 +38,7 @@ struct Node* merge(struct Node* listA, struct Node* listB) {
         tail = tail->next;
     }
 
-    // Append the remaining nodes of list1 or list2
-    if (listA != NULL) {
+        if (listA != NULL) {
         tail->next = listA;
     } else {
         tail->next = listB;
@@ -55,7 +48,7 @@ struct Node* merge(struct Node* listA, struct Node* listB) {
 }
 
 int main() {
-    // Create two sorted linked lists
+    
     struct Node* listA = createNode(3);
     listA->next = createNode(9);
     listA->next->next = createNode(15);
@@ -64,16 +57,14 @@ int main() {
     listB->next = createNode(12);
     listB->next->next = createNode(18);
 
-    // Print the original lists
-    printf("List A: ");
     printList(listA);
     printf("List B: ");
     printList(listB);
 
-    // Merge the lists
+   
     struct Node* mergedList = merge(listA, listB);
 
-    // Print the merged list
+   
     printf("Merged List: ");
     printList(mergedList);
 
